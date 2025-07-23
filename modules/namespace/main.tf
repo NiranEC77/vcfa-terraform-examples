783da317690a6e5b0d60e081b8156c3de71ecf3b
niranec77 <nmiranec@gmail.com>
2025-07-23 16:32:23 -0400

    changed strg limit to 1000mi on ns

--- modules/namespace/main.tf
+++ modules/namespace/main.tf
@@ -7,7 +7,7 @@ resource "vcfa_supervisor_namespace" "supervisor_namespace" {
   vpc_name     = var.vpc_name
 
   storage_classes_initial_class_config_overrides {
-    limit = "100Mi"
+    limit = "1000Mi"
     name  = "vcfa-storage-policy"
   }
 
