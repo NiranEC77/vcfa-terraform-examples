b44276ffe7395bc79a37b2647ccc5f1fde70c19b
niranec77 <nmiranec@gmail.com>
2025-07-23 16:18:23 -0400

    changes storage policy on vm

--- virtual-machine/main.tf
+++ virtual-machine/main.tf
@@ -18,7 +18,7 @@ data "vcfa_region" "region" {
 
 data "vcfa_storage_class" "sc" {
   region_id = data.vcfa_region.region.id
-  name      = "vSAN Default Storage Policy"
+  name      = "vvcfa-storage-policy"
 }
 
 resource "vcfa_content_library" "cl" {
